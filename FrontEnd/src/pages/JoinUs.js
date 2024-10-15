3ffa951110f94b359f98b7754c69591cc6fc726d
Dennis <hlam3@cs.washington.edu>
2024-10-15 16:14:57 -0700

    Update Join Us page

--- FrontEnd/src/pages/JoinUs.js
+++ FrontEnd/src/pages/JoinUs.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import staffImage from '../assets/Staff.jpg';
+import StaffImage from "../assets/Staff.jpg";
 
 const JoinUs = () => {
     return (
         <div>
             <h1><center>Staff</center></h1>
-            <img src={staffImage} alt="Staff" title="Staff" style={{display: 'block', margin: '0 auto'}}/>
+            <img src={StaffImage} alt="Staff" title="Staff" style={{display: 'block', margin: '0 auto'}}/>
         </div>
 
     );
