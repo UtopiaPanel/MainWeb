e406879293a12297da79f970693d611f12303c9a
Dennis <hlam3@cs.washington.edu>
2024-10-15 16:15:12 -0700

    Update Gallery, add new photo

--- FrontEnd/src/pages/Gallery.js
+++ FrontEnd/src/pages/Gallery.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import GroupPhoto1 from '../assets/西雅图新生会Staff.jpg';
 
 const Gallery = () => {
     return (
         <div>
             <h1>Gallery</h1>
             <p>
-                <img src="https://via.placeholder.com/150" alt="Placeholder" title="Placeholder"/>
-                <img src="https://via.placeholder.com/150" alt="Placeholder" title="Placeholder"/>
+                <img src={GroupPhoto1} alt="Staff" style={{width: '33.33vw'}}/>
                 <img src="https://via.placeholder.com/150" alt="Placeholder" title="Placeholder"/>
             </p>
         </div>
