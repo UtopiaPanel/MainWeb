03bfa9b26c55c557687fb434ec5f2fb92f4cc910
Dennis Lam <hinyueng.lam@gmail.com>
2024-11-26 23:42:57 -0800

    Update navbar: combine upcoming and past events items

--- FrontEnd/src/components/Navbar.js
+++ FrontEnd/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../style/Navbar.css';
 import logo from '../assets/logo.png'
